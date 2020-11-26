71223878d1c2215f18e01a94eebf9d30b7654e6c
Kishor <kishor1124@gmail.com>
2020-11-26 17:09:09 +0530

    encryption & decryption examples

--- config-client-demo/src/main/java/com/example/configclientdemo/ConfigClientDemoApplication.java
+++ config-client-demo/src/main/java/com/example/configclientdemo/ConfigClientDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.configclientdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+
 public class ConfigClientDemoApplication {
 
 	public static void main(String[] args) {
