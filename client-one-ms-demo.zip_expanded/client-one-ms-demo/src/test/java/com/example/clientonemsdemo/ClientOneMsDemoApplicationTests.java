6e4d8731ef5f62dfc1e56c5be89895ecb9b5097c
Kishor <kishor1124@gmail.com>
2020-11-30 16:28:45 +0530

    eureka server & client

---
+++ client-one-ms-demo.zip_expanded/client-one-ms-demo/src/test/java/com/example/clientonemsdemo/ClientOneMsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientonemsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientOneMsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
